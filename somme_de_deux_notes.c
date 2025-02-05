374b64459f580e808f96473567b6a9cc9f506b04
Mandresy308 <ramparanantenainamandresy@gmail.com>
2025-02-05 20:47:32 +0300

    Add files via upload

---
+++ somme_de_deux_notes.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    float a,b,s;    
+    printf("Veiller entrer le premier note  :  "); 
+    scanf("%f",&a);
+    printf("Veiller entrer le second note   :  ");
+    scanf("%f", &b);
+    
+    s=a+b;
+    
+    printf("La somme est  :  %f\n", s);
+    
+    return(0);
+
+
+
+
+}
